2820b1efe312cfd06133550fff91a46d0da95972
meenmuf <51811331+meenmuf@users.noreply.github.com>
2019-06-28 12:02:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if ( userText== 'สวัสดี') :
+    sendText(user,('เอออ ดีด้วย')
+    elif (userText == 'ไอ้บ้า') :
+    sendText(user,('มึงอะบ้า')
+    elif (userText == 'เหมือนหล่ออะ') :
+    sendText(user,('ควยไร')
     return '',200
-
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
